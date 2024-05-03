db8bb65262e6a649e30f4515e0eccd281483f657
javiprietod <javierpridom@gmail.com>
2024-05-04 01:21:49 +0200

    [CODE] - Frontend for delete user

--- mi-app-react/src/Profile.jsx
+++ mi-app-react/src/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
-export default function Register() {
+export default function profile() {
 
     const [nombre, setNombre] = useState('');
     const [tel, setTel] = useState('');
@@ -66,6 +66,35 @@ export default function Register() {
     };
 
 
+    const handleDeletion = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        const formData = {
+            nombre: nombre,
+            tel: tel,
+        };
+        console.log(formData);
+        const fetchData = async () => {
+            fetch('http://localhost:8000/api/users/me/', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    credentials: 'include',
+                }).then((res) => {
+                    if (confirm('Are you sure you want to delete your account?')) {
+                        console.log(res.status);
+                        if (res.ok) {
+                            location.href = '/';
+                        }
+                    }
+                }).catch((error) => {
+                console.log(error.message, 'error');
+            });
+        };
+        fetchData();
+    };
+
+
     return (<div className="container">
     <h2>Update profile</h2>
     
@@ -78,6 +107,8 @@ export default function Register() {
             <input type="submit" value="Update profile information" />
         </form> 
         <button onClick={() => location.href = '/password'}>Change Password</button>
+        <hr />
+        <button onClick={handleDeletion}>Delete account</button>
   </div>
   )
 };
