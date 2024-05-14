a2c7476abd540813b68e26ed056884f5f1394ecb
javiprietod <javierpridom@gmail.com>
2024-05-14 11:57:18 +0200

    [CODE] - API doc started

--- api/users/views.py
+++ api/users/views.py
@@ -284,19 +284,17 @@ class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
 
 @api_view(["POST"])
 def movie_bulk_create(request):
-    if request.method == "POST":
-        data = request.data
-        if not isinstance(data, list):
-            return Response(
-                {"error": "Request body must be a list"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        serializer = serializers.MovieSerializer(data=data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    data = request.data
+    if not isinstance(data, list):
+        return Response(
+            {"error": "Request body must be a list"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    serializer = serializers.MovieSerializer(data=data, many=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ReviewList(generics.ListCreateAPIView):
